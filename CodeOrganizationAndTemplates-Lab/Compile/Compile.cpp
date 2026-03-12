94da061a2511101c9466f37b088220d97e9903e3
Dopovski <157282507+Dopovski@users.noreply.github.com>
2026-03-12 18:33:28 +0200

    Create Compile.cpp

---
+++ CodeOrganizationAndTemplates-Lab/Compile/Compile.cpp
@@ -0,0 +1,13 @@
+#include "Defines.h"
+
+int main() {
+#ifndef DONT_COMPILE_THIS
+
+#endif
+	using STANDARD_TEMPLATE_LIBRARY;
+	string input;
+	cin >> input;
+	cout << input << endl;
+
+	return 0;
+}
